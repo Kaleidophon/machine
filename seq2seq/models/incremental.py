b489e3ab53ca415514224467f615df28ef252e26
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-09-21 11:38:16 +0200

    Implement the Anticipating Encoder class, adding placeholder code for the corresponding trainer.

--- seq2seq/models/incremental.py
+++ seq2seq/models/incremental.py
@@ -6,14 +6,27 @@ linguistic information more incrementally and therefore closer to the way that h
 from .EncoderRNN import EncoderRNN
 from .DecoderRNN import DecoderRNN
 
+from torch import nn
+
 
 class AnticipatingEncoderRNN(EncoderRNN):
     """
     Special kind of encoder which tries to also predict the next token of the sequence, where wrong predictions are
     penalized with a loss function similar to the predictions of a decoder.
     """
-    # TODO: Implement
-    pass
+    def __init__(self, vocab_size, max_len, hidden_size, embedding_size, input_dropout_p=0, dropout_p=0, n_layers=1,
+                 bidirectional=False, rnn_cell='gru', variable_lengths=False):
+        super().__init__(
+            vocab_size, max_len, hidden_size, embedding_size, input_dropout_p, dropout_p, n_layers,bidirectional,
+            rnn_cell, variable_lengths
+        )
+        self.prediction_layer = nn.Linear(hidden_size, vocab_size)  # Layer to predict next token in input sequence
+
+    def forward(self, input_var, input_lengths=None):
+        output, hidden = super().forward(input_var, input_lengths)
+        prediction = self.prediction_layer(hidden)
+
+        return output, hidden, prediction
 
 
 class BottleneckDecoderRNN(DecoderRNN):
